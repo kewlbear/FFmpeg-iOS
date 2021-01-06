e73823d6ca25c88e7008ecd8f6a959098df3e623
Changbeom Ahn <kewlbear@gmail.com>
2021-01-06 11:15:48 +0900

    Test remote XCFramework

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
     targets: [
-        .binaryTarget(name: "avcodec", path: "Frameworks/avcodec.xcframework"),
+        .binaryTarget(name: "avcodec", url: "https://github.com/kewlbear/FFmpeg-iOS/releases/download/0.0.2/codec.zip", checksum: "50fc7c75f51600f49539cef20826008813ff91722fe1c2269d6fb949621e56ae"),
         .binaryTarget(name: "avutil", path: "Frameworks/avutil.xcframework"),
         .binaryTarget(name: "avformat", path: "Frameworks/avformat.xcframework"),
         .binaryTarget(name: "avfilter", path: "Frameworks/avfilter.xcframework"),
